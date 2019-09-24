01e739306670e98721e9fcf6d7d45156dad972c5
alannorman99 <asnorman3604@eagle.fgcu.edu>
2019-09-23 20:43:36 -0400

    test commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+  //test commit
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
