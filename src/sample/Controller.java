3910e9ed5d99839c5d595aff2e593f95a654d903
Alan Norman <asnorman3604@eagle.fgcu.edu>
2019-09-28 15:32:42 -0400

    Added CSS to full GUI need to continue testing before final submit To Do: JavaDoc

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javax.swing.DefaultComboBoxModel;
@@ -25,6 +26,21 @@ import javax.swing.DefaultComboBoxModel;
  * @author Alan Norman
  */
 public class Controller {
+
+  @FXML
+  private Label lblChooseQuantity;
+
+  @FXML
+  private Label lblChooseProduct;
+
+  @FXML
+  private Tab tabProductLine;
+
+  @FXML
+  private Tab tabProduce;
+
+  @FXML
+  private Tab tabProductionLog;
 
   @FXML
   private Button btnRecordProduction;
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -23,7 +23,10 @@ public class Main extends Application {
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
     primaryStage.setTitle("Hello World");
-    primaryStage.setScene(new Scene(root, 300, 275));
+    Scene scene = new Scene(root, 300, 275);
+    scene.getStylesheets().add(Main.class.getResource("productCSS.css").toExternalForm());
+    primaryStage.setScene(scene);
+
     primaryStage.show();
   }
 
