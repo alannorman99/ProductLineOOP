065a50659c19c170be8c3261331a087a3d9c0b11
Alan Norman <asnorman3604@eagle.fgcu.edu>
2019-09-27 20:15:16 -0400

    Fully Styled with google Styling CheckSytle found zero issues Findbugs only shows issue with a blank DB password To Do: -Add Comments and add anything left from the rubric

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,105 +1,112 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
 
-import java.sql.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+
 
 public class Controller {
 
-    public Tab tab_Produce;
-    @FXML
-    private Button btn_RecordProduction;
+  //public Tab tabProduce;
+  @FXML
+  private Button btnRecordProduction;
 
-    @FXML
-    private TextField txtF_ProductName;
+  @FXML
+  private TextField txtfProductName;
 
-    @FXML
-    private TextField txtF_Manufacturer;
+  @FXML
+  private TextField txtfManufacturer;
 
-    @FXML
-    private ChoiceBox<?> cB_ItemType;
+  @FXML
+  private ChoiceBox<?> cbItemType;
 
-    @FXML
-    private ComboBox<String> cmBox_Quantity;
+  @FXML
+  private ComboBox<String> cmBoxQuantity;
 
 
+  private Connection conn = null;
+  private Statement stmt = null;
 
-    private Connection conn = null;
-    private Statement stmt = null;
+  @FXML
+  private Button btnAddProduct;
 
-    @FXML
-    private Button btn_AddProduct;
+  public void initialize() {
+    System.out.println("Initialize");
+  }
 
-    public void initialize() {
-        System.out.println("Initialize");
-    }
+  //Creates a connection to the database
+  private void initializeDB() {
 
-    //Creates a connection to the database
-    private void initializeDB() {
+    //Driver and Location of Database
+    final String Jdbc_Driver = "org.h2.Driver";
+    final String Db_Url = "jdbc:h2:./res/ProductionDB";
 
-        //Driver and Location of Database
-         final String JDBC_DRIVER = "org.h2.Driver";
-         final String DB_URL = "jdbc:h2:./res/ProductionDB";
+    //  Database credentials
+    final String User = "";
+    final String Pass = "";
 
-        //  Database credentials
-         final String USER = "";
-         final String PASS = "";
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(Jdbc_Driver);
 
-        try {
-            // STEP 1: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(Db_Url, User, Pass);
 
-            //STEP 2: Open a connection
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-
-        }
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
 
     }
 
-    //Used to confirm user input within the Produce Tab
-    @FXML
-    void recordButtonAction(ActionEvent event) {
+  }
 
-        System.out.println("Record Production");
+  //Used to confirm user input within the Produce Tab
+  @FXML
+  void recordButtonAction(ActionEvent event) {
+
+    System.out.println("Record Production");
+  }
+
+  //Button used to add a product to the database
+  @FXML
+  void onAddProductPress(ActionEvent event) {
+
+    //calls initializeDB method
+    initializeDB();
+
+    try {
+      //uses the connection from initializeDB to create a statement
+      stmt = conn.createStatement();
+
+      //SQL statement used to access database
+      String sql = "INSERT INTO Product(type, manufacturer, name) "
+          + "VALUES ( 'AUDIO', 'Apple', 'iPod' )";
+
+      //executes the previous steps
+      stmt.executeUpdate(sql);
+
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    //Button used to add a product to the database
-    @FXML
-    void onAddProductPress(ActionEvent event) {
-
-        //calls initializeDB method
-        initializeDB();
-
-        try {
-           //uses the connection from initializeDB to create a statement
-            stmt = conn.createStatement();
-
-            //SQL statement used to access database
-            String sql = "INSERT INTO Product(type, manufacturer, name) " +
-                    "VALUES ( 'AUDIO', 'Apple', 'iPod' )";
-
-            //executes the previous steps
-            stmt.executeUpdate(sql);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        // STEP 4: Clean-up environment
-        try {
-            stmt.close();
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+    // STEP 4: Clean-up environment
+    try {
+      stmt.close();
+      conn.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    }
+  }
+
+}
 
 
